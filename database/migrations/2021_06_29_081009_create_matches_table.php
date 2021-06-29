305b24f6bbf4f6fa50dc9c4563d85cb5204fd646
Norudah <pierucci.romain@gmail.com>
2021-06-29 18:16:20 +0200

    fix precedent commit

---
+++ database/migrations/2021_06_29_081009_create_matches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date_at');
+            $table->foreignId('monster_id')->constrained('monsters');
+            $table->foreignId('arena_id')->constrained('arenas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match');
+    }
+}
